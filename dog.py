fdddc5f32b0b1ae36243d70e9c8dd8f38474d3a9
Marty Umlas <martyumlas@gmail.com>
2024-02-28 19:09:27 +0800

    initial commit

---
+++ dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    '''a simple attemp to model a dog'''
+    def __init__(self, name, age):
+        '''initialize name and age attribute'''
+        self.name = name
+        self.age = age
+    def sit(self):
+        # return f'{self.name} is now sitting'
+        print(f'{self.name} is now sitting')
+
+
+dog = Dog('zooey', 10)
+
+print(dog.name, dog.age)
+dog.sit()
