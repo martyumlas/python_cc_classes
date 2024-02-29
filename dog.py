2a7a064f5cd8d85a439f690af94f09cbe4a698d4
martyumlas <martyumlas@gmail.com>
2024-02-29 13:26:48 +0800

    Dog class

--- dog.py
+++ dog.py
@@ -4,9 +4,13 @@ class Dog:
         '''initialize name and age attribute'''
         self.name = name
         self.age = age
+
     def sit(self):
         # return f'{self.name} is now sitting'
         print(f'{self.name} is now sitting')
+
+    def roll_over(self):
+        print((f'{self.name} rolled over !'))
 
 
 dog = Dog('zooey', 10)
